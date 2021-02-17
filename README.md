81a3d144087048041bb06e3e01e7317171ba5487
Henry Zhou <henryzhou9469@gmail.com>
2021-02-18 10:06:52 +1100

    Change arbitrage calculation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # triangular-arbitrage
+This program connects to BTC Markets WebSocket V2 and \
+subscribes to the ETH-AUD, ETH-BTC, and BTC-AUD tickers, \
+looking for a triangular arbitrage opportunity among the trading pairs.
+
+### Usage
+Create npm project and install relevant packages. Run `websocket.js`
--- websocket.js
+++ websocket.js
@@ -3,7 +3,7 @@ const WebSocket = require('ws');
 
 const baseUrl = 'wss://socket.btcmarkets.net/v2';
 const channels = ['tick', 'heartbeat'];
-const marketIds = ['BTC-AUD']; // ['BTC-AUD', 'ETH-AUD', 'ETH-BTC'];
+const marketIds = ['BTC-AUD', 'ETH-AUD', 'ETH-BTC'];
 
 // if using private channels then set api key and secret for authentication
 const key = undefined;
@@ -11,17 +11,16 @@ const secret = 'add your API key secret here';
 
 const ws = new WebSocket(baseUrl);
 
-var request = {
+let request = {
     marketIds: marketIds,
     channels: channels,
     messageType: 'subscribe'
 }
 
-var rateETHAUD;
-var rateETHBTC;
-var rateBTCAUD;
-var fiat = 10000;
-var tradingFee = 0.00
+let rateETHAUD = null;
+let rateETHBTC = null;
+let rateBTCAUD = null;
+let tradingAmount = 100;
 
 if (key) {
   const now = Date.now();
@@ -40,17 +39,14 @@ ws.on('message', function incoming(data) {
   jsonData = JSON.parse(data)
   switch (jsonData["messageType"]) {
     case "heartbeat":
-      console.log("heartbeat");
+      //console.log("heartbeat");
       break;
     case "tick":
-      console.log("tick");
       processTick(jsonData);
-      //console.log(data);
-      //ws.terminate()
       break;
     default:
-      var code = jsonData["code"]
-      var msg = jsonData["message"]
+      let code = jsonData["code"]
+      let msg = jsonData["message"]
       console.error(`ERROR!! ${code} --- ${msg}`);
       ws.terminate()
   }
@@ -65,42 +61,48 @@ ws.on('error', function error(err) {
 });
 
 function signMessage(secret, message) {
-  var key = Buffer.from(secret, 'base64');
-  var hmac = crypto.createHmac('sha512', key);
-  var signature = hmac.update(message).digest('base64');
+  let key = Buffer.from(secret, 'base64');
+  let hmac = crypto.createHmac('sha512', key);
+  let signature = hmac.update(message).digest('base64');
   return signature;
 }
 
 function processTick(data) {
-  var marketId = data["marketId"];
-  var bestAsk = parseFloat(data["bestAsk"]);
-  var lastPrice = parseFloat(data["lastPrice"]);
+  let marketId = data["marketId"];
+  let bestBid = parseFloat(data["bestBid"]);
+  let bestAsk = parseFloat(data["bestAsk"]);
+  let lastPrice = parseFloat(data["lastPrice"]);
 
   if (marketId === "ETH-AUD") {
     rateETHAUD = (bestAsk < lastPrice) ? bestAsk : lastPrice;
   }
   if (marketId === "ETH-BTC") {
-    rateETHBTC = bestAsk;
+    rateETHBTC = bestBid;
   }
   if (marketId === "BTC-AUD") {
-    rateBTCAUD = bestAsk;
+    rateBTCAUD = bestBid;
   }
 
-  var turnover = arbitrage(rateETHAUD, rateETHBTC, rateBTCAUD)
-  if (turnover > 1) {
-    console.log("Arbitrage Opportunity: " + turnover)
+console.log("rateETHAUD: " + rateETHAUD + " rateETHBTC: " + rateETHBTC + " rateBTCAUD: " + rateBTCAUD);
+
+  if (rateETHAUD != null && rateETHBTC != null && rateBTCAUD != null) {
+    let turnover = calculateArbitrageOpportunity(rateETHAUD, rateETHBTC, rateBTCAUD, tradingAmount);
+    if (turnover > 1) {
+      console.log("Arbitrage Opportunity: " + turnover);
+    } else {
+      console.log("No Arbitrage Opportunity: " + turnover);
+    }
   } else {
-    console.log("No Arbitrage Opportunity")
+    console.log("Insufficient data");
   }
 }
 
-function arbitrage(rate1, rate2, rate3, fiat, tradingFee=0.0085, makerTaker=0, reversed=true) {
-  calculation = fiat * (1 - tradingFee) / rate1;
-  calculation = (reversed) ?
-                (calculation * (1 - makerTaker) * rate2) :
-                (calculation * (1 - makerTaker) / rate2);
-  calculation = calculation * (1 - tradingFee) * rate3;
-  calculation = calculation - fiat;
-
-  return calculation;
+function calculateArbitrageOpportunity(ex1, ex2, ex3, tradingAmount, tradingFee=0.0085, takerFee=0.002) {
+    //normal   : e.g. AUD --> ETH, ETH --> BTC, BTC --> AUD
+    //reversed : e.g. AUD --> BTC, BTC --> ETH, ETH --> AUD
+    let ethAmount = tradingAmount/ex1;
+    let btcAmount = ethAmount*ex2;
+    let audAmount = btcAmount*ex3;    
+    let totalFees = tradingAmount*tradingFee + ethAmount*takerFee*ex1 + btcAmount*tradingFee*ex3;
+    return audAmount - tradingAmount - totalFees;
 }
