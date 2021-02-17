1c32857ccba4fad3bf3e428628392264171aee12
Henry Zhou <henryzhou9469@gmail.com>
2021-02-17 22:37:08 +1100

    Websocket reading arbitrage

--- websocket.js
+++ websocket.js
@@ -17,6 +17,12 @@ var request = {
     messageType: 'subscribe'
 }
 
+var rateETHAUD;
+var rateETHBTC;
+var rateBTCAUD;
+var fiat = 10000;
+var tradingFee = 0.00
+
 if (key) {
   const now = Date.now();
   const strToSign =  "/users/self/subscribe" + "\n" + now;
@@ -31,7 +37,23 @@ ws.on('open', function open() {
 });
 
 ws.on('message', function incoming(data) {
-  console.log(data);
+  jsonData = JSON.parse(data)
+  switch (jsonData["messageType"]) {
+    case "heartbeat":
+      console.log("heartbeat");
+      break;
+    case "tick":
+      console.log("tick");
+      processTick(jsonData);
+      //console.log(data);
+      //ws.terminate()
+      break;
+    default:
+      var code = jsonData["code"]
+      var msg = jsonData["message"]
+      console.error(`ERROR!! ${code} --- ${msg}`);
+      ws.terminate()
+  }
 });
 
 ws.on('close', function close() {
@@ -47,4 +69,38 @@ function signMessage(secret, message) {
   var hmac = crypto.createHmac('sha512', key);
   var signature = hmac.update(message).digest('base64');
   return signature;
+}
+
+function processTick(data) {
+  var marketId = data["marketId"];
+  var bestAsk = parseFloat(data["bestAsk"]);
+  var lastPrice = parseFloat(data["lastPrice"]);
+
+  if (marketId === "ETH-AUD") {
+    rateETHAUD = (bestAsk < lastPrice) ? bestAsk : lastPrice;
+  }
+  if (marketId === "ETH-BTC") {
+    rateETHBTC = bestAsk;
+  }
+  if (marketId === "BTC-AUD") {
+    rateBTCAUD = bestAsk;
+  }
+
+  var turnover = arbitrage(rateETHAUD, rateETHBTC, rateBTCAUD)
+  if (turnover > 1) {
+    console.log("Arbitrage Opportunity: " + turnover)
+  } else {
+    console.log("No Arbitrage Opportunity")
+  }
+}
+
+function arbitrage(rate1, rate2, rate3, fiat, tradingFee=0.0085, makerTaker=0, reversed=true) {
+  calculation = fiat * (1 - tradingFee) / rate1;
+  calculation = (reversed) ?
+                (calculation * (1 - makerTaker) * rate2) :
+                (calculation * (1 - makerTaker) / rate2);
+  calculation = calculation * (1 - tradingFee) * rate3;
+  calculation = calculation - fiat;
+
+  return calculation;
 }
